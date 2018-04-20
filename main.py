0dc7438a6d0074a88d4d5a598be3f688ddbfa9c0
Anders <Anders2108@gmail.com>
2018-04-20 15:13:20 +0200

    Added spidev example

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-import RPi.GPIO as GPIO
+import spidev
+import time
 
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 7629
+
+def write_pot(input):
+    msb = input >> 8
+    lsb = input & 0xFF
+    spi.xfer([msb, lsb])
+
+while true:
+    write_pot(0x1FF)
+    time.sleep(0.5)
+    write_pot(0x00)
+    time.sleep(0.5)
+    
