e2f8561b07788a1173af4fe6f5a1a05efed14d40
Bakhoj <Anders2108@gmail.com>
2018-05-09 13:28:09 +0200

    DataPack added
    Will function as the data object which can be used to pass to another class such as aws.py

---
+++ data_pack.py
@@ -0,0 +1,30 @@
+from simple_time import SimpleTime
+
+class DataPack():
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.station_id = None
+        self.time = SimpleTime()
+
+        self.has_bat_lvl = False
+        self.has_air_temp = False
+        self.has_air_hum = False
+
+        self.bat_lvl = None
+        self.air_temp = None
+        self.air_hum = None
+
+        self.dict = None
+    
+    def buildDict(self):
+        self.dict = dict()
+        if self.has_bat_lvl:
+            self.dict["batteryLevel"] = self.bat_lvl
+        if self.has_air_temp:
+            self.dict["airTemperatur"] = self.air_temp
+        if self.has_air_hum:
+            self.dict["airHumidity"] = self.air_hum
+
+    
