56e4108548681f8df2b49d7209102cf2ae7d4462
Bakhoj <Anders2108@gmail.com>
2018-05-09 13:28:53 +0200

    simple_time.py added float time element

--- simple_time.py
+++ simple_time.py
@@ -1,3 +1,4 @@
+import time
 
 class SimpleTime():
 
@@ -5,13 +6,18 @@ class SimpleTime():
         self.day = day
         self.hour = hour
         self.minute = minute
+        self.time = None
 
     def byte_to_time(self, first = 0, second = 0):
         self.day = ((first & 0xF8) >> 3)
         self.hour = (((first & 0x07) << 2) + ((second & 0xC0) >> 6))
         self.minute = (second & 0x3F)
+        self.time = time.time()
 
     def print_time(self):
         print("Day: \t", self.day)
         print("Hour: \t", self.hour)
         print("Min: \t", self.minute)
+
+    def to_float_time(self):
+        return self.time
