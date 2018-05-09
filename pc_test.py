c3f9a81fea533a04b37417be2c0e5952f1ad6747
Bakhoj <Anders2108@gmail.com>
2018-05-09 13:28:34 +0200

    pc_test.py uploads to DB now

--- pc_test.py
+++ pc_test.py
@@ -1,4 +1,5 @@
 from weather_master import PackageReader
+from aws import AWS
 
 print("Start Master Module Simulator")
 
@@ -12,4 +13,9 @@ payload = [0x0A, 0x05, 0x2E, 0x95, 0xF3, 0x71, 0x84, 0x00, 0x62, 0x11]
 print(bytes(payload).hex())
 
 reader.read_package(payload, True)
-print("Accepted payload: ", reader.is_accepted())
+
+db = AWS()
+
+db.connect()
+db.publish_sensor_data(reader.data_pack, True)
+db.disconnect()
